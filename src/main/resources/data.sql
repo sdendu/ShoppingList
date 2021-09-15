9f57a246352136bb82d7286c81b722a0a92e636b
Duncan, Tyson <Tyson.Duncan@McKesson.com>
2021-09-15 13:15:47 -0500

    initial schema

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,13 @@
-DROP TABLE IF EXISTS billionaires;
+DROP TABLE IF EXISTS items;
 
-CREATE TABLE billionaires (
+CREATE TABLE items (
   id INT AUTO_INCREMENT  PRIMARY KEY,
-  first_name VARCHAR(250) NOT NULL,
-  last_name VARCHAR(250) NOT NULL,
-  career VARCHAR(250) DEFAULT NULL
+  name VARCHAR(250) NOT NULL,
+  description VARCHAR(250) NOT NULL,
+  category VARCHAR(250) DEFAULT NULL
 );
 
-INSERT INTO billionaires (first_name, last_name, career) VALUES
-  ('Aliko', 'Dangote', 'Billionaire Industrialist'),
-  ('Bill', 'Gates', 'Billionaire Tech Entrepreneur'),
-  ('Folrunsho', 'Alakija', 'Billionaire Oil Magnate');
+INSERT INTO items (name, description, category) VALUES
+  ('Milk', 'MMMMM Milk is good!!', 'KITCHEN'),
+  ('Shoes', 'Fancy new shoes', 'CLOTHING'),
+  ('Red Stapler', 'Have you seen it?', 'OFFICE');
