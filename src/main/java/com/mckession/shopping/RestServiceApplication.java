85927a4066ab2492970b0a7056a300a95473c4f1
dendu <dendu@DESKTOP-5DKVFMC>
2021-09-15 13:55:44 -0500

    getAll api working

--- src/main/java/com/mckession/shopping/RestServiceApplication.java
+++ src/main/java/com/mckession/shopping/RestServiceApplication.java
@@ -1,4 +1,4 @@
-package com.mckession.shopping.restservice;
+package com.mckession.shopping;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/mckession/shopping/restservice/repo/ItemRepository.java
+++ src/main/java/com/mckession/shopping/restservice/repo/ItemRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface ItemRepository extends CrudRepository<Item, Integer> {
+public interface ItemRepository extends JpaRepository<Item, Integer> {
 }
